79a408afb56bb4936eebc1c3b816f99399deec0c
rick <ricardogeuzabiaga@gmail.com>
2024-04-25 01:13:03 -0600

    qmc almost done

--- qmc.py
+++ qmc.py
@@ -10,9 +10,9 @@ def group_rows(rows:list)->dict:
     for row in rows:
         b_on = bits_on(row)
         if b_on in group:
-            group[b_on].append(row)
+            group[b_on].append([[row],True,row])
         else:
-            group[b_on] = [row]
+            group[b_on] = [[[row],True,row]]
     return group
 
 def compare_and_chage(b1:str,b2:str)->str:
@@ -28,26 +28,36 @@ def compare_and_chage(b1:str,b2:str)->str:
             new_b = new_b + b1[i]
     return new_b
 
-def look_for_matched_pairs(group:dict,size:int):
+def look_for_matched_pairs(group:dict,size:int)->dict:
     pairs = {}
     for i in range(1,size+1):
         if (i in group) and ((i-1) in group):
             pairs[i-1] = []
             for row in group[i-1]:
                 for b in group[i]:
-                    r = compare_and_chage(row,b)
+                    r = compare_and_chage(row[-1],b[-1])
                     if r != None:
-                        pairs[i-1].append((row,b,r))
+                        pairs[i-1].append([row[0] + b[0],True,r])
+                        row[1] = False
+                        b[1] = False
+            if len(pairs[i-1]) == 0:
+                del pairs[i-1]
     return pairs
 
 
 def quine_mcCluskey(minterms:list,size:int):
     rows = [int_to_str_bin(minterm,size) for minterm in minterms]
     first_group = group_rows(rows)
-    print(first_group)
+    lists = [first_group]
+
     pairs = look_for_matched_pairs(first_group,size)
-    print(pairs)
+    while len(pairs) > 0:
+        lists.append(pairs)
+        pairs = look_for_matched_pairs(pairs,size)
+    #create a simplification function for the last list, it may not have more than one key
 
+    print("<<<<>>>>")
+    print(lists)
 
-quine_mcCluskey([5,12,16,19,20,2,13,0],5)
-print(compare_and_chage('x00','x10'))
+#quine_mcCluskey([5,12,16,19,20,2,13,0],5)
+quine_mcCluskey([0,2,3,4,6],3)
